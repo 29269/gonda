0559fc7f9f04961c3de0abeee5b9e93b567fcf13
Duneya <54691201+29269@users.noreply.github.com>
2019-11-13 09:28:58 +0100

    Add files via upload

---
+++ gonda/inlog.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title id ="title">log in</title>
+  <meta charset="UTF-8">
+  <meta name="description" content="Portfolio">
+  <meta name="keywords" content="Portfolio">
+  <meta name="author" content=" Gonda">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="style.css">
+</head>
+<header>
+
+  <a href="home.html"><img src="final.png" alt="logo" width="10%"></a>
+</header>
+<body>
+  <div class="about-text">
+  <form action="welcome1.php" method="post">
+    <img src="person.png", width="20%">
+  <p>User name:</p>
+   <input type="text" name="user" /><br>
+   <p>E-mail address:</p>
+ <input type="text" name="email" /><br>
+   <p>Password:</p>
+   <input type="password" name="password" /><br>
+
+  <p class="inlog"> <button name="submit" type="submit">Log in</button></p>
+  </form>
+</div>
+</body>
+</html>
---
+++ gonda/welcome1.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+  <title id ="title">inlog</title>
+  <meta charset="UTF-8">
+  <meta name="description" content="Portfolio">
+  <meta name="keywords" content="Portfolio">
+  <meta name="author" content=" Gonda">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="style.css">
+</head>
+<header>
+  <a href="home.html"><img src="final.png", width="10%", text-align= "center"></a>
+</header>
+<body>
+<div class="about-text">
+  <img src="de.png", width="50%">
+<p>Welcome :<?php echo $_POST["user"]; ?></p>
+  ___________________________________
+<p>Your email address is: <?php echo $_POST["email"]; ?></p>
+</div>
+</body>
+</html>
